c6e2158f4b425264247e5d16fdc80fdf488cb01e
akhsyz <faaiz.akhsya.tif424@polban.ac.id>
2025-05-17 11:11:59 +0700

    struktur awal

---
+++ stack.h
@@ -0,0 +1,24 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// Struktur data buat nyimpen hasil pertandingan 
+
+typedef struct {
+  int matchID     // ID pertandingan 
+  int team1Id;    // ID tim 1
+  int team2Id;    // ID tim 2
+  int idPemenang; // ID pemenang 
+  int nomorRonde; // Nomor ronde/babak
+} MatchResult;
+
+// Node buat stack 
+typedef struct StackNode{
+  MatchResult data;
+  struct StackNode* next;
+} StackNode;
+
+// Struktur Stack
