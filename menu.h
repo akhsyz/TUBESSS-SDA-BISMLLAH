8a1560aa6f005649225ee5e73014191b8034a04d
Belva Abthal Hidayat <belva.a.hidayat@gmail.com>
2025-05-22 00:01:16 +0700

    definisi awal header modul-modul menu

---
+++ menu.h
@@ -0,0 +1,23 @@
+/*
+PIC : belva 
+NIM : 241524035
+*/
+
+#ifndef MENU_H
+#define MENU_H
+
+#include "fileio.h"
+
+void uiGreetings();
+void mainMenu();
+void showsTambahTim();
+void showsHapusTim();
+void showsDaftarTim();
+void showsJadwalkanPertandingan();
+void showsInputResult();
+void showsUndoResult();
+void showsBracket();
+void showsStatistik();
+void exitProgram();
+
+#endif
