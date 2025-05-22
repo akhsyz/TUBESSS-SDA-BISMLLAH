2ddcde742cea9374f68b6320b08b0310da81322d
akhsyz <faaiz.akhsya.tif424@polban.ac.id>
2025-05-22 17:09:08 +0700

    update stack.c

--- stack.c
+++ stack.c
@@ -16,29 +16,67 @@ bool apakahStackKosong(Stack* s) {
   return (s->top == NULL);
 }
 
-// Push data ke stack 
+// Tambah riwayat pertandingan ke stack
 void push (Stack* s, MatchResult data) {
-
+  StackNode* newNode = (StackNode*)malloc(sizeof(StackNode));
+  if (!newNode)
+  {
+    printf("Error: Gagal alokasi memori untuk StackNode\n");
+    return;
+  }
+  newNode->data = data;
+  newNode->next = s->top;
+  s->top = newNode;
+  s->size++;
 }
 
-// Pop data dari stack 
+// Hapus dan kembalikan pertandingan terakhir
 bool pop (Stack* s, MatchResult* result){
-
+  if (apakahStackKosong(s))
+  {
+    return false;
+  }
+  StackNode* temp = s->top;
+  *result = temp->data;
+  s->size--;
+  return true;
 }
 
-// Lihat data teratas tanpa ngambil 
+// Lihat pertandingan teratas tanpa menghapus 
 bool peek(Stack* s, MatchResult* result){
-
+  if (apakahStackKosong(s))
+  {
+    return false;
+  }
+  *result = s->top->data;
+  return true;
 }
 
-// Bersihin semua isi stack 
+// Kosongkan semua isi stack 
 void clearStack(Stack* s){
-
+  StackNode*s current = s->top;
+  while (current != NULL)
+  {
+    StackNode* temp = current;
+    current = current->next;
+    free(temp);
+  }
+  s->top = NULL;
+  s->size = 0;
 }
 
 // Mendapatkan ukuran stack
 int getStackSize(Stack* s){
+  return s->size;
+}
+
+// Tampilkan riwayat pertandingan
+void tampilkanHistori(){
 
 }
 
+// Batalkan pertandingan terakhir dan kembalikan tim ke Queue
+void undoMatch(){
+
+}
 
