3f41cb58211ec1ac9849b58af528158a2504b828
akhsyz <faaiz.akhsya.tif424@polban.ac.id>
2025-05-28 11:42:10 +0700

    struct awal buat statistik

---
+++ results.h
@@ -0,0 +1,23 @@
+#ifndef RESULTS_H
+#define RESULTS_H
+
+#include "stack.h"
+
+#define MAX_TIM 16
+#define MAX_NAMA 50
+
+// Struktur untuk menyimpan statistik tim 
+typedef struct {
+  char namaTim[MAX_NAMA];
+  int laga;
+  int menang;
+  int kalah:
+} StatistikTim;
+
+// Deklarasi fungsi 
+void tampilkanStatistik(StackNode* s, StackNode head);
+void saveHasilkeFile(StatistikTim tim[], int teamCount, const char* filename);
+
+#endif
+
+
