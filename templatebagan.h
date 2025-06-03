03d9bb8934f6235c44ba270c405c8193f5b345fb
danishaufaa <muhammad.danish.tif424@polban.ac.id>
2025-06-03 17:12:16 +0700

    membuat template bagan

---
+++ templatebagan.h
@@ -0,0 +1,18 @@
+#ifndef TEMPLATEBAGAN_H
+#define TEMPLATEBAGAN_H
+
+void templatebagan4tim(char teams[][50], int scores[], char* winner);
+void templatebagan5tim(char teams[][50], int scores[], char* winner);
+void templatebagan6tim(char teams[][50], int scores[], char* winner);
+void templatebagan7tim(char teams[][50], int scores[], char* winner);
+void templatebagan8tim(char teams[][50], int scores[], char* winner);
+void templatebagan9tim(char teams[][50], int scores[], char* winner);
+void templatebagan10tim(char teams[][50], int scores[], char* winner);
+void templatebagan11tim(char teams[][50], int scores[], char* winner);
+void templatebagan12tim(char teams[][50], int scores[], char* winner);
+void templatebagan13tim(char teams[][50], int scores[], char* winner);
+void templatebagan14tim(char teams[][50], int scores[], char* winner);
+void templatebagan15tim(char teams[][50], int scores[], char* winner);
+void templatebagan16tim(char teams[][50], int scores[], char* winner);
+
+#endif
