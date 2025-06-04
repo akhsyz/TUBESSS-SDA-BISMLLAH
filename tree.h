bfca5a2a52cb4194e0afb827b39d48036e333857
Belva Abthal Hidayat <belva.a.hidayat@gmail.com>
2025-06-04 10:52:25 +0700

    update struktur data tree

--- tree.h
+++ tree.h
@@ -1,7 +1,18 @@
 #ifndef TREE_H
 #define TREE_H
 
-#include "templatebagan.h"
+#include "queue.h"
+
+typedef struct NodeTree {
+    int id_tim1; // ID tim pertama (0 jika BYE)
+    int id_tim2; // ID tim kedua (0 jika BYE)
+    int id_pemenang; // ID pemenang (diisi setelah pertandingan)
+    int match_id; // ID pertandingan (seed)
+    struct NodeTree* left;
+    struct NodeTree* right;
+} NodeTree;
+
+typedef NodeTree* addressTree;
 
 void initTree();
 void generateTournamentTree(int teamCount, char teams[][50], int scores[], char* winner);
