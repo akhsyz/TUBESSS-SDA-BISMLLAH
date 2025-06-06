df5470c666baea7fb97eba0f8c780c7cba77caf1
akhsyz <faaiz.akhsya.tif424@polban.ac.id>
2025-06-07 05:55:57 +0700

    revisi stack.h

--- results.h
+++ results.h
@@ -5,9 +5,9 @@
 
 // Deklarasi fungsi 
 void tampilkanStatistik(StackNode* s, StackNode head);
-void saveHasilkeFile(StatistikTim tim[], int teamCount, const char* filename);
-void inputResult(addressTree *tournamentTree, addressList head, Stack *matchHistory);
-void undoResult(addressTree *tournamentTree, addressList head, Stack *matchHistory);
+void tampilkanHistori(Stack *s);
+void inputMatchResult(addressTree *turnamentTree, addressList head, Stack *matchHistory );
+void undoMatchResult (addressTree *turnmanetTree, addressList head, Stack *matchHistory);
 void updateMatchResult(addressTree root, int match_id, int id_pemenang, int skor1, int skor2, addressList teams, Stack *history);
 
 #endif
