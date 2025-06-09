f0d5e04faa210b1d39ab104e3dd908d3c1819613
danishaufaa <muhammad.danish.tif424@polban.ac.id>
2025-06-09 13:58:23 +0700

    update isi fileio.h

--- fileio.h
+++ fileio.h
@@ -1,6 +1,13 @@
 #ifndef FILEIO_H
 #define FILEIO_H
 
-#include "tournament.h"
+#include "templatebagan.h"
+
+void saveTeamsToFile(addressList head, const char* filename);
+void saveMatchHistoryToFile(Stack* history, const char* filename);
+void saveTournamentTreeToFile(addressTree root, const char* filename);
+addressList loadTeamsFromFile(const char* filename);
+Stack* loadMatchHistoryFromFile(const char* filename);
+addressTree loadTournamentTreeFromFile(const char* filename);
 
 #endif
